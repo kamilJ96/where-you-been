0740fd814e6d4643959739cb39e6c9eec6c2ffb7
Kam <kjakrzewski@student.unimelb.edu.au>
2018-04-28 19:11:17 +1000

    optimize filter function for better grouping

--- filter_history.py
+++ filter_history.py
@@ -24,7 +24,7 @@ new_data['locations'] = []
 gym_lat = -37873
 gym_lon = 144726
 end_time = 0
-max_elapsed_time = 14400000
+max_elapsed_time = 10800000
 
 def conv_timestamp(time):
     new_time = datetime.datetime.fromtimestamp(time/1000.0)
@@ -39,25 +39,35 @@ def constr_long(lon):
 # check each data point and see if it matches the necessary coordinates
 # if it does, consider that the end time
 for location in data['locations']:
+    if not end_time:
+        timestamps = []
+
     lon = int(str(location['longitudeE7'])[:6])
     lat = int(str(location['latitudeE7'])[:6])
+
     if (gym_lat-1 <= lat <= gym_lat+1) and (gym_lon-1 <= lon <= gym_lon+1):
         if not end_time:
             end_time = int(location['timestampMs'])
+        else:
+            prev_location = location
+
     elif end_time:
-        start_time = int(prev_location['timestampMs'])
-        if end_time - start_time > 0:
+        if end_time - int(location['timestampMs']) > max_elapsed_time:
+            '''
+            try:
+                prev_location = timestamps[-1]
+            except IndexError:
+                continue
+                '''
+            start_time = int(prev_location['timestampMs'])
+            
             new_data['locations'].append({
                 'start_time': start_time,
-                'end_time': end_time,
-                'longitude': prev_location['longitudeE7'],
-                'latitude' : prev_location['latitudeE7'],
                 'start_date': conv_timestamp(start_time),
+                'end_time': end_time,
                 'end_date': conv_timestamp(end_time)
                 })
-        end_time = 0
-
-    prev_location = location
+            end_time = 0
 
 json.dump(new_data, new_file, indent=4)
 new_file.close()
